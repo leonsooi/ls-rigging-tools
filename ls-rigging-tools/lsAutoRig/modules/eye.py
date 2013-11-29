aa9ef9f69be98eb0802ff5315ec9a6f453194769
leonsooi@gmail.com <leonsooi@gmail.com@594fc2ac-ca58-9de2-a7fb-9aca50f43ac4>
2013-11-29 03:53:05 +0000

    construct eyelid rig - create crv, joints, ikhdl, attach to curve.

--- ls-rigging-tools/lsAutoRig/modules/eye.py
+++ ls-rigging-tools/lsAutoRig/modules/eye.py
@@ -2,10 +2,53 @@ import time
 
 import pymel.core as pm
 
+import cgm.lib.deformers as cgmDeformers
+
 import lsAutoRig.lib.mesh as mesh
 reload(mesh)
 import lsAutoRig.lib.datatypes as data
 reload(data)
+
+def constructEyelidsRig():
+    prefix = 'CT_'
+    # select edge loop
+    # ... [user]
+    crv = pm.polyToCurve(form=1, degree=3, ch=False)[0]
+    crv = pm.PyNode(crv)
+    crv.rename(prefix + 'eyelidAim_crv_0')
+    
+    # add locators to crv
+    for param in range(crv.numSpans()):
+        loc = pm.spaceLocator(n=crv.replace('_crv','_loc').replace('_0','_%d'%param))
+        poci = pm.createNode('pointOnCurveInfo', n=crv.replace('_crv_','_poci_').replace('_0','_%d'%param))
+        crv.worldSpace >> poci.inputCurve
+        poci.position >> loc.translate
+        poci.parameter.set(param)
+        loc.localScale.set(0.2,0.2,0.2)
+
+    # get pivot - center of eye
+    pivotgeo = 'pivot_geo'
+    pivotgeo = pm.PyNode(pivotgeo)
+    eyePivotVec = pivotgeo.rotatePivot.get()
+    
+    # create joints
+    eyeAimJnts = []
+    
+    for param in range(crv.numSpans()):
+        pm.select(cl=True)
+        baseJnt = pm.joint(n=crv.replace('_crv_','_jnt_').replace('_0','_%d'%param), p=eyePivotVec)
+        endPivotVec = crv.cv[param].getPosition()
+        endJnt = pm.joint(n=crv.replace('_crv_','_bnd_').replace('_0','_%d'%param), p=endPivotVec)
+        baseJnt.orientJoint('xyz')
+        endJnt.jointOrient.set(0,0,0)
+        ikHdl, ikEff = pm.ikHandle(n=baseJnt.replace('_jnt_', '_hdl_'), sj=baseJnt, ee=endJnt, solver='ikSCsolver')
+        loc = pm.PyNode(baseJnt.replace('_jnt_', '_loc_'))
+        loc | ikHdl
+    
+        eyeAimJnts.append(baseJnt)
+        eyeAimJnts.append(endJnt)
+    
+    [jnt.radius.set(0.3) for jnt in eyeAimJnts]
 
 def findChildren(root, orphans):
     '''
