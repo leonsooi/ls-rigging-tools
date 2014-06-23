63110dfeb0aab17dc6c40f5a2a8e13f234c4ab7d
leonsooi@gmail.com <leonsooi@gmail.com@594fc2ac-ca58-9de2-a7fb-9aca50f43ac4>
2014-06-23 01:28:01 +0000

    priCtl.addOffset allows setting transform order so that offset pivots around offsetted-position.

--- ls-rigging-tools/rigger/modules/priCtl.py
+++ ls-rigging-tools/rigger/modules/priCtl.py
@@ -4,9 +4,12 @@ Created on May 26, 2014
 @author: Leon
 '''
 import pymel.core as pm
-def addOffset(pCtl, hierarchy, suffix='_offset'):
+def addOffset(pCtl, hierarchy, order='secondLast', suffix='_offset'):
     '''
     hierarchy - 'parent', 'child', 'sibling', 'world'
+    order - 'secondLast', 'second'
+    secondLast will make offset pivot from original position
+    second will make offset pivot from offset's post-transform position
     '''
     offsetGrp = pm.group(em=True, n=pCtl+suffix)
     wMat = pCtl.getMatrix(worldSpace=True)
@@ -24,20 +27,39 @@ def addOffset(pCtl, hierarchy, suffix='_offset'):
         pass
     else:
         pm.warning('Unknown hierarchy method: %s' % hierarchy)
-        
+    
     # connect matrices to drive children
     outMMs = pCtl.matrix.outputs(type='multMatrix')
-    for outMM in outMMs:
-        # be sure to use the next plugIndex
-        nextIndex = outMM.matrixIn.numElements()
-        nextPlug = outMM.matrixIn.elementByLogicalIndex(nextIndex)
-        lastPlug = outMM.matrixIn.elementByLogicalIndex(nextIndex-1)
-        invMat = lastPlug.inputs(p=True)[0]
-        if not nextPlug.isConnected():
-            # offsetGrp matrix should be inserted at second-last plug (invMat)
-            offsetGrp.matrix >> lastPlug
-            invMat >> nextPlug
-        else:
-            pm.warning('Overriding plug %s. Check logical indices.' % nextPlug)
-            
+        
+    if order == 'secondLast':
+
+        for outMM in outMMs:
+            # be sure to use the next plugIndex
+            nextIndex = outMM.matrixIn.numElements()
+            nextPlug = outMM.matrixIn.elementByLogicalIndex(nextIndex)
+            lastPlug = outMM.matrixIn.elementByLogicalIndex(nextIndex-1)
+            invMat = lastPlug.inputs(p=True)[0]
+            if not nextPlug.isConnected():
+                # offsetGrp matrix should be inserted at second-last plug (invMat)
+                offsetGrp.matrix >> lastPlug
+                invMat >> nextPlug
+            else:
+                pm.warning('Overriding plug %s. Check logical indices.' % nextPlug)
+    
+    elif order == 'second':
+        for outMM in outMMs:
+            # move elements up by 1
+            # (keep element 0 at 0, since we're going to insert at 1)
+            nextIndex = outMM.matrixIn.numElements()
+            plugsToMove = [outMM.matrixIn.elementByLogicalIndex(i).inputs(p=True)[0]
+                           for i in range(1, nextIndex)]
+            # move plugs
+            for plugId, plug in enumerate(plugsToMove):
+                plug >> outMM.matrixIn.elementByLogicalIndex(plugId+2)
+                
+            # connect offsetGrp to second plug
+            offsetGrp.matrix >> outMM.matrixIn[1]
+    
+    else:
+        pm.warning('Unknown order method %s' % order)
     return offsetGrp
